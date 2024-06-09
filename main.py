d000035777cfb46b4d963beeb200b9018d68cc11
muq33 <muqq33@gmail.com>
2024-06-09 11:45:01 -0300

    -Versão utilizável

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import schedule
 from tokenHandler.tokenHandler import *
 from ah2Sql.ah2Sql import *
+import time
 
 def main(token_acesso, motor) -> None:
     if not token_acesso.is_valid():
@@ -17,8 +18,8 @@ if __name__ == '__main__':
     
     token_acesso = tokenHandler('https://oauth.battle.net/token', 'dadc296d33ad4d89b461625e765dab61', 'IhTLcEUksRNtisRQKwylUmBf91teqZZH')
     token_acesso.get_access_token()
-    #print(token_acesso.ACCESS_TOKEN)
     motor = ah2Sql('mysql', 'pymysql', 'root', '', 'localhost', '3306', 'ah2sql', 'data', token_acesso.ACCESS_TOKEN)
-    motor.extract_data()
-    #main(token_acesso=token_acesso, motor=motor)
-    #schedule.every(2).hours.do(main)
+    schedule.every(2).hours.do(main, token_acesso, motor)
+    while True:
+        schedule.run_pending() 
+        time.sleep(5) 
